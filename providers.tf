b12a92a36c38d973d752f8eb7dc9da4f0fcde0fe
Ketan Bhagat <ketanbhagat@live.com>
2022-07-08 17:39:39 -0400

    resolve

--- providers.tf
+++ providers.tf
@@ -1,5 +1,5 @@
 #WellspringCloud
-provider "wellspringcloud" {
+provider "aws" {
 	region = "us-east-1"
 	profile = "wellspringcloud"
 }
--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@
 terraform {
 	required_version = "1.2.4"
 	required_providers {
-		wellspringcloud = {
+		aws = {
 			source  = "registry.terraform.io/hashicorp/aws"
 			version = "~> 4.22"
 		}

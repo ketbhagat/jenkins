bcb8b894f826627016057eff81e64bf9b14f4b58
Ketan Bhagat <ketanbhagat@live.com>
2022-07-31 08:46:40 -0400

    update

--- ec2.tf
+++ ec2.tf
@@ -17,14 +17,16 @@ resource "aws_volume_attachment" "wsc-jenkins-ebs" {
 	instance_id = aws_instance.jenkins.id
 }
 
-/*
-resource "aws_instance" "jenkins2" {
+resource "aws_instance" "jenkins-agent" {
         ami = data.aws_ami.amazonaminame.id
         key_name = "AWS2022"
         vpc_security_group_ids = [ "sg-0f33dc7a7f2ba3c66" ]
-        instance_type =  "t2.micro"
-        user_data = "${file("install_jenkins.sh")}"
+        instance_type =  "t2.micro"	
+	availability_zone = "us-east-1b"
+	user_data = "${file("script.sh")}"
+	private_ip = "172.31.80.80"
+       # user_data = "${file("install_jenkins.sh")}"
         tags  =  {
-                        Name = "jenkins2"
+                        Name = "jenkins-agent"
                 }
-} */
+}
